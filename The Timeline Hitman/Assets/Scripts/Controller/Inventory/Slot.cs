3647232dc0a97045327e3d142fed8adb95afaf4d
Vitr3x <ryanbett88@gmail.com>
2020-01-31 23:29:26 +0000

    Updated HUD
    + Created basic hud layout
    
    No functionality

---
+++ The Timeline Hitman/Assets/Scripts/Controller/Inventory/Slot.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+
+    public bool empty = false;
+
+}
---
+++ The Timeline Hitman/Assets/Scripts/Model/ItemProperties.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Item Desc
+    private string ItemDescription;
+    // Item Picture
+    private Texture2D ItemIcon;
+
+}
