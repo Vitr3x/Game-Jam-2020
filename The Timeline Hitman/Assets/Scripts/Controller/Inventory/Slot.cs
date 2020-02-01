bf3a06a52d330271395416bca597b1a67e7fbe2b
V <ryanbett88@gmail.com>
2020-02-01 16:24:30 +0000

    Resetting Master
    Resetting master

---
+++ The Timeline Hitman/Assets/Scripts/Controller/Inventory/Slot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour
+{
+    private bool empty = true;
+
+    public Image icon;
+
+    public void setIcon(Sprite newIcon)
+    {
+        icon.sprite = newIcon;
+        icon.enabled = true;
+    }
+
+    public void clearIcon()
+    {
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+
+
+    public bool isEmpty()
+    {
+        return empty;
+    }
+
+    public void setEmpty(bool empty)
+    {
+        this.empty = empty;
+    }
+
+}
---
+++ The Timeline Hitman/Assets/Scripts/Model/ItemProperties.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // --- Possible Additions
+    // Item Name
+    // Item Description
+
+    // Item Picture
+    private Sprite ItemIcon;
+
+    public Sprite getItemIcon()
+    {
+        return ItemIcon;
+    }
+
+    public void setItemIcon(Sprite newIcon)
+    {
+        ItemIcon = newIcon;
+    }
+}
