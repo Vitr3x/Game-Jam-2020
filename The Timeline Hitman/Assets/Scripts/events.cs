6f1dcd9a5689a8ec4e6d7cadde36242b81f07026
Hsaunders2 <43548015+Hsaunders2@users.noreply.github.com>
2020-02-01 16:33:44 +0000

    Event Update

---
+++ The Timeline Hitman/Assets/Scripts/events.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class events : MonoBehaviour
+{
+    bool killedGuy = false;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void killedMan(string namez)
+    {
+        StartCoroutine(TimeIt(2, namez));
+    }
+
+    void killedManpart2()
+    {
+        Debug.Log("we made it");
+    }
+
+    IEnumerator TimeIt(int seconds, string namez)
+    {
+        yield return new WaitForSeconds(seconds);
+        if (namez == "Guy")
+        {
+            killedManpart2();
+        }
+    }
+}
--- The Timeline Hitman/Assets/Scripts/kill.cs
+++ The Timeline Hitman/Assets/Scripts/kill.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class kill : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    GameObject person;
+    GameObject eventers;
+    string namez = "";
+
     void Start()
     {
-        
+        person = this.gameObject;
+        namez = this.gameObject.name;
+        eventers = GameObject.FindGameObjectWithTag("Inventory");
+        Debug.Log(eventers);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        if (other.name == "bullet(Clone)")
+        {
+            Debug.Log("hit");
+            eventers.GetComponent<events>().killedMan(namez);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+
+        }
     }
 }
