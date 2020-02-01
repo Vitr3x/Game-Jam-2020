56db3bd66d1acffbd3a8eedaa837dadc8f3526d6
Hsaunders2 <43548015+Hsaunders2@users.noreply.github.com>
2020-02-01 16:39:54 +0000

    Resetting master

---
+++ The Timeline Hitman/Assets/Scripts/gunShooter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunShooter : MonoBehaviour
+{
+    public GameObject BulletSpawn;
+    public GameObject Bullet;
+    public float BulletSpeed;
+    bool shoot = true;
+
+
+    private float time = 0.0f;
+    public float interpolationPeriod = 0.5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        time += Time.deltaTime;
+
+
+
+        if (time >= interpolationPeriod)
+        {
+            time = 0.0f;
+            shoot = true;
+        }
+
+        if (shoot == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject tempBullet;
+                tempBullet = Instantiate(Bullet, BulletSpawn.transform.position, BulletSpawn.transform.rotation) as GameObject;
+                tempBullet.transform.Rotate(Vector3.left * 90);
+                Rigidbody RB;
+                RB = tempBullet.GetComponent<Rigidbody>();
+                RB.AddForce(transform.forward * BulletSpeed);
+                Destroy(tempBullet, 3);
+                shoot = false;
+            }
+        }
+    }
+}
