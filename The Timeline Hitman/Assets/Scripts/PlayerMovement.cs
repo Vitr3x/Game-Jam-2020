90dc55ba17882412753395da5eb644b3ed012e3f
Hsaunders2 <43548015+Hsaunders2@users.noreply.github.com>
2020-02-01 21:56:21 +0000

    pls push
    warawrf

---
+++ The Timeline Hitman/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController cc;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 0.5f;
+
+    public Transform grounded;
+    public float groundDist = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 banana;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(grounded.position, groundDist, groundMask);
+
+        if(isGrounded && banana.y < 0)
+        {
+            banana.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        cc.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            banana.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        banana.y += gravity * Time.deltaTime;
+
+        cc.Move(banana * Time.deltaTime);
+    }
+}
---
+++ The Timeline Hitman/Assets/Scripts/gunShooter.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunShooter : MonoBehaviour
+{
+    public GameObject BulletSpawn;
+    public GameObject Bullet;
+    public float BulletSpeed;
+    GameObject hand;
+    GameObject aim;
+
+    bool shoot = false;
+    GameObject cam;
+
+
+    private float time = 0.0f;
+    public float interpolationPeriod = 0.5f;
+    // Use this for initialization
+    void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        hand = GameObject.FindGameObjectWithTag("Hand");
+        aim = GameObject.FindGameObjectWithTag("Aim");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        time += Time.deltaTime;
+
+
+
+       
+
+        RaycastHit hit;
+        Physics.Raycast(cam.transform.position, cam.transform.forward, out hit);
+
+
+        if(hand.transform.childCount > 0)
+        {
+            for (int i = 0; i < hand.transform.childCount; i++)
+            {
+                if (hand.transform.GetChild(i).transform.gameObject.name == "gun")
+                {
+                    Debug.Log("we through");
+                    if (time >= interpolationPeriod)
+                    {
+                        time = 0.0f;
+                        shoot = true;
+                    }
+                    Vector3 diff = hand.transform.GetChild(i).transform.position - aim.transform.position;
+                    hand.transform.GetChild(i).transform.rotation = Quaternion.LookRotation(diff);
+                    hand.transform.GetChild(i).transform.Rotate(0, 180, 0);
+                }
+            }
+        }
+
+        if (shoot == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                GameObject tempBullet;
+                tempBullet = Instantiate(Bullet, BulletSpawn.transform.position, BulletSpawn.transform.rotation) as GameObject;
+                tempBullet.transform.Rotate(Vector3.left * 90);
+                Rigidbody RB;
+                RB = tempBullet.GetComponent<Rigidbody>();
+                RB.AddForce(transform.forward * BulletSpeed);
+                Destroy(tempBullet, 3);
+                shoot = false;
+            }
+        }
+    }
+}
